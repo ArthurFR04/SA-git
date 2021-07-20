27f54da93e7bbe950e1faf033f4ae97f1a7b8c36
Thinhague <thinhague@outlook.com>
2021-07-20 14:34:33 -0300

    validaçao

--- SA-arquivos/js/cadastro.js
+++ SA-arquivos/js/cadastro.js
@@ -4,68 +4,123 @@
 cadastros = []
 
 function cadastrar() {
-    let vapo = 3
+
+
+
+
+
     let nomeC = document.getElementById('inpNomeC').value   // atribui o valor dos inputs à variáveis          // Editável
     let emailC = document.getElementById('inpEmailC').value // atribui o valor dos inputs à variáveis          // Editável
     let senhaC = document.getElementById('inpSenhaC').value // atribui o valor dos inputs à variáveis          // Editável
     let senha2C = document.getElementById('inpSenha2C').value // atribui o valor dos inputs à variáveis          // Editável
+
+    let form = document.querySelector('.validator');
+    let inputs = form.querySelectorAll('input')
+    let li = document.querySelectorAll('li');
+
+    clear();
+    for (let i = 0; i < inputs.length; i++) {
+        let input = inputs[i];
+
+
+
+        if (input.value == '') {
+            input.style.border = 'solid 2px'
+            input.style.borderColor = 'red'
+            li[i].innerHTML = 'Preencha todos os campos!'
+
+
+        }
+
+    }
+    function clear() {
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+            li[i].innerHTML = '';
+
+
+
+        }
+    }
     
+
+
+    // if(nomeC == ''|| emailC == '' || senhaC == '' || senha2C == '' ){
+    //     if(nomeC == '' || nomeC == null){
+    //         nomeC = document.getElementById('inpNomeC').style.border = 'solid 2px'
+    //         inputs[0].style.borderColor = 'red'
+    //         textErro1.innerHTML = 'Este campo deve ser preenchido!'
+    //     }
+    //     if(emailC == '' || emailC == null){
+    //         emailC = document.getElementById('inpEmailC').style.border = 'solid 2px'
+    //         emailC = document.getElementById('inpEmailC').style.borderColor = 'red'
+    //         textErro2.innerHTML = 'Este campo deve ser preenchido!'
+    //     }
+
+
+    //     return false
+
+
+    // }
+
+
+
     if (senhaC === senha2C) {
 
         let confirmacao = ''                      //usei para verificar o array, o for percorre e muda o valor se for true
 
         nomeDoObj = {                                             // cria um ojeto
-            apelido:nomeC,                                                                                   // Editável
-            email:emailC,                                                                                    // Editável
-            senha:senhaC,                                                                                    // Editável
+            apelido: nomeC,                                                                                   // Editável
+            email: emailC,                                                                                    // Editável
+            senha: senhaC,                                                                                    // Editável
         }
-        
+
         if (localStorage.getItem('Pessoas') === null) {  // Adicionando um array com um objeto no localstorage
 
             cadastros.push(nomeDoObj)
             localStorage.setItem('Pessoas', JSON.stringify(cadastros)); // O JSON.parse transforma a string em JSON novamente, o inverso do JSON.strigify
 
             alert('Cadastro efetuado. \n\nVocê será redirecionado para a página de login.')
-                
+
             document.getElementById('inpNomeC').value = ''                                                 // Editável
             document.getElementById('inpEmailC').value = ''                                                // Editável
             document.getElementById('inpSenhaC').value = ''                                                // Editável
             document.getElementById('inpSenha2C').value = ''                                               // Editável
-                
-            window.location.href="tela_login.html"                                                         // Editável
+
+            window.location.href = "tela_login.html"                                                         // Editável
         }
 
-        else if (localStorage.getItem('Pessoas') != null){
-            
+        else if (localStorage.getItem('Pessoas') != null) {
+
             let cadastros2 = JSON.parse(localStorage.getItem('Pessoas'))
 
-            for(let i=0; i<cadastros2.length; i++) {
+            for (let i = 0; i < cadastros2.length; i++) {
 
                 if (cadastros2[i].apelido == nomeC || cadastros2[i].email == emailC) {                                                          // Editável
                     confirmacao = 'cancelar'
                 }
             }
-                
+
             if (confirmacao === 'cancelar') {
                 console.log('deu igual');
                 alert('Esse nome de usuário já está sendo usado.')
                 return false;
             }
-            
+
             else {
                 console.log('deu diferente');
-                
+
                 cadastros2.push(nomeDoObj)
                 localStorage.setItem('Pessoas', JSON.stringify(cadastros2))
-                
+
                 alert('Cadastro efetuado. \n\nVocê será redirecionado para a página de login.')
-                
+
                 document.getElementById('inpNomeC').value = ''                                                 // Editável
                 document.getElementById('inpEmailC').value = ''                                                // Editável
                 document.getElementById('inpSenhaC').value = ''                                                // Editável
                 document.getElementById('inpSenha2C').value = ''                                               // Editável
-                
-                window.location.href="tela_login.html"                                                         // Editável
+
+                window.location.href = "tela_login.html"                                                         // Editável
                 return false;
             }
         }
