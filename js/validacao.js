1ed4f62768ad6d1821c85997fec0b9e04872412b
ArthurSenai <arthur_f_rodrigues@estudante.sesisenai.org.br>
2021-07-23 15:11:59 -0300

    código js arrumado, cagada desfeitas :)

--- js/validacao.js
+++ js/validacao.js
@@ -3,15 +3,17 @@
 
 function cadastrar() {
 
-    let form = document.querySelector('.validator');//seleciona a tag form
-    let inputs = form.querySelectorAll('input')//seleciona todos os inputs do form
-    let li = document.querySelectorAll('li');// seleciona todas as tags li
-    let p = document.querySelectorAll('p');//seleciona todas as tags p
-    let a = document.querySelectorAll('a');//seleciona todas as tags a
+    let form = document.querySelector('.validator');    //seleciona a tag form
+    let inputs = form.querySelectorAll('input');        //seleciona todos os inputs do form
+    let li = document.querySelectorAll('li');           // seleciona todas as tags li
+    let p = document.querySelectorAll('p');             //seleciona todas as tags p
+    let a = document.querySelectorAll('a');             //seleciona todas as tags a
 
     //VALIDAÇOES DOS ERROS
     let send = true;
+
     clear();
+    
     for (let i = 0; i < inputs.length; i++) {
         let input = inputs[i];
 
@@ -24,8 +26,8 @@ function cadastrar() {
         }
 
         if (inputs[2].value !== inputs[3].value) {
+
             inputs[3].style.border = 'red solid 2px'
-     
             li[3].innerHTML = 'As senhas devem ser iguais!'
 
             send = false;
@@ -38,7 +40,7 @@ function cadastrar() {
     if (cadastros2 !== null) {
         for (let i = 0; i<cadastros2.length; i++) {
 
-            if (cadastros2[i].apelido == nomeC) {                                                          // Editável
+            if (cadastros2[i].apelido == inputs[0].value) {                                                   // Editável
 
                 inputs[0].style.border = 'red solid 2px'
         
@@ -51,7 +53,7 @@ function cadastrar() {
                 send = false;
             }
 
-            if (cadastros2[i].email == emailC) {                                                          // Editável
+            if (cadastros2[i].email == inputs[1].value) {                                                     // Editável
 
                 inputs[1].style.border = 'red solid 2px'
 
@@ -69,38 +71,36 @@ function cadastrar() {
     if (send !== true) {
         return false
     }
+
     //FUNCÕES
     function clear() {
-        for (let i = 0; i < inputs.length; i++) {       //FUNCÃO LIMPAR INPUTS
+        for (let i = 0; i < inputs.length; i++) {       //FUNCÃO LIMPAR OS ERROS DOS INPUTS
             inputs[i].style = '';
             li[i].innerHTML = '';
-
         }
     }
 
-    function redirecionandoPage() {
+    function redirecionarPage() {
         window.location.href = "tela_login.html"      //FUNÇÃO REDIRECIONAR PÁGINA
     }
+
     function cadastroEfetuado() {
 
+        setInterval(redirecionarPage, 3500)
         p[1].innerHTML = `Cadastro efetuado!<br>Redirecionado...`         //FUNÇÃO CADASTRO EFETUADO
         a[1].innerHTML = '';
 
+        for (let i = 0; i < inputs.length; i++) {       //FUNCÃO LIMPAR OS VALORES DOS INPUTS
+            inputs[i].value = '';
+        }
     }
 
     cadastros = []
-    
-    let nomeC = document.getElementById('inpNomeC').value   // atribui o valor dos inputs à variáveis          // Editável
-    let emailC = document.getElementById('inpEmailC').value // atribui o valor dos inputs à variáveis          // Editável
-    let senhaC = document.getElementById('inpSenhaC').value // atribui o valor dos inputs à variáveis          // Editável
-    let senha2C = document.getElementById('inpSenha2C').value // atribui o valor dos inputs à variáveis          // Editável
-
-                    //usei para verificar o array, o for percorre e muda o valor se for true
 
     nomeDoObj = {                                             // cria um ojeto
-        apelido:nomeC,                                                                                   // Editável
-        email:emailC,                                                                                    // Editável
-        senha:senhaC,                                                                                    // Editável
+        apelido:inputs[0].value,                                                                              // Editável
+        email:inputs[1].value,                                                                                // Editável
+        senha:inputs[3].value,                                                                                // Editável
     }
     
     if (cadastros2 === null) {  // Adicionando um array com um objeto no localstorage
@@ -108,29 +108,19 @@ function cadastrar() {
         cadastros.push(nomeDoObj)
         localStorage.setItem('Pessoas', JSON.stringify(cadastros)); // O JSON.parse transforma a string em JSON novamente, o inverso do JSON.strigify
             
-        document.getElementById('inpNomeC').value = ''                                                 // Editável
-        document.getElementById('inpEmailC').value = ''                                                // Editável
-        document.getElementById('inpSenhaC').value = ''                                                // Editável
-        document.getElementById('inpSenha2C').value = ''                                               // Editável
-            
+        cadastroEfetuado()
+        clear()
     }
 
     else if (cadastros2 !== null) {
         cadastros2.push(nomeDoObj)
-            localStorage.setItem('Pessoas', JSON.stringify(cadastros2))
-    } 
 
-            cadastroEfetuado()
+        localStorage.setItem('Pessoas', JSON.stringify(cadastros2))
 
-            document.getElementById('inpNomeC').value = ''                                                 // Editável
-            document.getElementById('inpEmailC').value = ''                                                // Editável
-            document.getElementById('inpSenhaC').value = ''                                                // Editável
-            document.getElementById('inpSenha2C').value = ''                                               // Editável
-
-            setInterval(redirecinandoPage, 3500)         //redireciona a página                                          
-            return false;
-
-        
+        cadastroEfetuado()
+        clear()
+    }
+    
 }
 
 
