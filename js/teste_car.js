72b7f09b61ec589fc08d0d6b530b2010af1bad8b
ArthurSenai <arthur_f_rodrigues@estudante.sesisenai.org.br>
2021-08-12 15:50:24 -0300

    excluindo itens carrinho

--- js/teste_car.js
+++ js/teste_car.js
@@ -1,6 +1,10 @@
 
 let pessoas = JSON.parse(localStorage.getItem('Pessoas'))
 let Login = JSON.parse(localStorage.getItem('Login'))
+let position = Login.position
+let user_car = pessoas[position].carrinho
+
+
 
 function add_car(id) {
     
@@ -8,10 +12,6 @@ function add_car(id) {
         alert('para adicionar um produto ao carrinho, você primeiro deve efetuar o login.')
         return false
     }
-    
-    let position = Login.position
-
-    let user_car = pessoas[position].carrinho
 
     if (user_car.length < 1) {
 
@@ -26,7 +26,7 @@ function add_car(id) {
     }
     
 
-    let testeDoPapi = 0
+    let prodInCar = 0
 
     for (let i=0; i<user_car.length; i++) {
 
@@ -34,11 +34,13 @@ function add_car(id) {
 
             pessoas[position].carrinho[i].quantidade++
             localStorage.setItem('Pessoas', JSON.stringify(pessoas))
-            testeDoPapi = 1
+            prodInCar = 1
+
+            return false
         }
     }
     
-    if (testeDoPapi == 0) {
+    if (prodInCar == 0) {
 
         let prod_car = {
             id: id,
@@ -54,64 +56,29 @@ function add_car(id) {
 }
 
 
+function rem_car(id) {
 
 
+    for (let i=0; i<user_car.length; i++) {
 
-// function add_car(id) {
-//     let carrinho = JSON.parse(localStorage.getItem('carrinho'))
-//     let id_prod = id
+        if (user_car[i].id == id) {
 
-//     for (i=0; i<produtos.length; i++) {
+            pessoas[position].carrinho[i].quantidade--
+            localStorage.setItem('Pessoas', JSON.stringify(pessoas))
+        }
+        
+        if (pessoas[position].carrinho[i].quantidade == 0 || pessoas[position].carrinho[i].quantidade < 1) {
 
-//         if (produtos[i].id == id_prod) {
+            exc_car(id)
+        }
+    }
+}
 
-//             if (carrinho != null) {
-//                 let confirma = 0
+function exc_car(id) {
 
-//                 for (ii=0; ii<carrinho.length; ii++) {
-
-//                     if (carrinho[ii].id == id_prod) {
-
-//                         carrinho[ii].quant_carr++
-//                         localStorage.setItem('carrinho', JSON.stringify(carrinho))
-//                         confirma = 1
-//                         return false
-//                     }
-//                 }
-//                 if (confirma == 0) {
-
-//                     let car2 = JSON.parse(localStorage.getItem('carrinho'))
-
-//                     car2.push(produtos[i])
-//                     localStorage.setItem('carrinho', JSON.stringify(car2))
-//                     return false
-//                 }
-                
-//             }
-//             else {
-//                 carrinho = []
-//                 carrinho.push(produtos[i])
-//                 localStorage.setItem('carrinho', JSON.stringify(carrinho))
-//             }
-//         }
-//     }
-// }
-
-// function rem_car(id) {
-//     let carrinho = JSON.parse(localStorage.getItem('carrinho'))
-//     let nome_prod = nome
-
-//     for (ii=0; ii<carrinho.length; ii++) {
-
-//         if (carrinho[ii].nome == nome_prod) {
-
-//             carrinho[ii].quant_carr--
-//             localStorage.setItem('carrinho', JSON.stringify(carrinho))
-//             // confirma = 1
-//             return false
-//         }
-//     }
-// } 
+    pessoas[position].carrinho.splice(i, 1)
+    localStorage.setItem('Pessoas', JSON.stringify(pessoas))
+}
 
 
 
