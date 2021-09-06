8cf537c99b96d094797cdecd8ca5074b8f0c4076
thinhague <thinhague@outlook.com>
2021-09-06 14:49:37 -0300

    agoraVai

--- js/admin/admin3.js
+++ js/admin/admin3.js
@@ -1,6 +1,7 @@
 let inputs = document.querySelectorAll('input')
 let li = document.querySelectorAll('li')
 var produtoCadastro = JSON.parse(localStorage.getItem('Produto'))
+var produto = JSON.parse(localStorage.getItem('produtos'))
 
 
 
@@ -26,42 +27,78 @@ function localizarProduto() {
         return false;
     }
 
-    for (let i = 0; i < produtoCadastro.length; i++) {
+    if (produtoCadastro == undefined) {
+        send1 = true
+    } else {
+        for (let i = 0; i < produtoCadastro.length; i++) {
 
-        if (produtoCadastro[i].nome == inputs[0].value || produtoCadastro[i].codigo == inputs[0].value) {
-            
-            send1 = false;
-        } 
+            if (produtoCadastro[i].nome == inputs[0].value || produtoCadastro[i].id == inputs[0].value) {
 
-        
+                send1 = false;
+            }
+
+
+        }
     }
+
+
+    for (let i = 0; i < produto.length; i++) {
+
+        if (produto[i].nome == inputs[0].value || produto[i].id == inputs[0].value) {
+
+            send1 = false;
+        }
+
+
+    }
+
     if (send1 == true) {
         li[0].innerHTML = 'Esse produto não existe!'
         inputs[0].style.border = 'red solid 2px'
         inputs[1].value = ''
         inputs[2].value = ''
         inputs[3].value = ''
-        
+
 
         send = false;
-    } 
-    
+    }
+
 
     if (send !== true) {
         return false;
     }
 
+    if (produtoCadastro == undefined) {
+
+    } else {
+        for (let i = 0; i < produtoCadastro.length; i++) {
+
+            if (produtoCadastro[i].nome == inputs[0].value || produtoCadastro[i].id == inputs[0].value) {
 
 
-    for (let i = 0; i < produtoCadastro.length; i++) {
+                inputs[0].value = ''
+                inputs[1].value = produtoCadastro[i].nome
+                inputs[2].value = produtoCadastro[i].id
+                inputs[3].value = produtoCadastro[i].preco
 
-        if (produtoCadastro[i].nome == inputs[0].value || produtoCadastro[i].codigo == inputs[0].value) {
+            }
 
-            
+
+        }
+    }
+
+
+
+
+    for (let i = 0; i < produto.length; i++) {
+
+        if (produto[i].nome == inputs[0].value || produto[i].id == inputs[0].value) {
+
+
             inputs[0].value = ''
-            inputs[1].value = produtoCadastro[i].nome
-            inputs[2].value = produtoCadastro[i].codigo
-            inputs[3].value = produtoCadastro[i].preco
+            inputs[1].value = produto[i].nome
+            inputs[2].value = produto[i].id
+            inputs[3].value = produto[i].preco
 
         }
 
@@ -75,8 +112,11 @@ let indice = null
 
 function removerProduto() {
 
+if (produtoCadastro == undefined) {
+
+} else {
     for (let i = 0; i < produtoCadastro.length; i++) {
-        if (produtoCadastro[i].nome == inputs[1].value && produtoCadastro[i].codigo == inputs[2].value && produtoCadastro[i].preco == inputs[3].value) {
+        if (produtoCadastro[i].nome == inputs[1].value && produtoCadastro[i].id == inputs[2].value && produtoCadastro[i].preco == inputs[3].value) {
 
             indice = i;
             produtoCadastro.splice(indice, 1);
@@ -86,7 +126,28 @@ function removerProduto() {
             inputs[2].value = ''
             inputs[3].value = ''
 
-            Swal.fire ({
+            Swal.fire({
+                icon: 'success',
+                title: 'Produto Removido!'
+            })
+        }
+    }
+}
+    
+
+    for (let i = 0; i < produto.length; i++) {
+        if (produto[i].nome == inputs[1].value && produto[i].id == inputs[2].value && produto[i].preco == inputs[3].value) {
+
+            indice = i;
+            produto.splice(indice, 1);
+            localStorage.setItem('produtos', JSON.stringify(produto))
+            
+
+            inputs[1].value = ''
+            inputs[2].value = ''
+            inputs[3].value = ''
+
+            Swal.fire({
                 icon: 'success',
                 title: 'Produto Removido!'
             })
